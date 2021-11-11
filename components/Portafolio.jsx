f12fff92d80bc1a7275a21ad3219cbee4905af5a
Felipe Lagos <felagossanchez@gmail.com>
2021-11-10 21:25:51 -0300

    config deploy to vercel

--- components/Portafolio.jsx
+++ components/Portafolio.jsx
@@ -9,20 +9,6 @@ export const Portafolio = () => {
         </h2>
         <div className="grid-portafolio">
           <div className="card">
-            <img src="/img/thumbails/thumbs-01.jpg" />
-            <div className="card-body">
-              <h5 className="card-title text-center">Petgram</h5>
-              <a
-                href="https://petgram.felipelagos.dev"
-                target="_blank"
-                className="btn btn-orange btn-large"
-              >
-                Visitar
-              </a>
-            </div>
-          </div>
-
-          <div className="card">
             <img src="/img/thumbails/thumbs-02.jpg" />
             <div className="card-body">
               <h5 className="card-title text-center">Moviedb</h5>
